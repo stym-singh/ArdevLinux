fcbddc3501e877af4fe6596182cae2dafabd9547
Satyam Singh <stymsingh@outlook.com>
2021-07-09 20:01:01 +0530

    restyle index.html and add defaults.html

--- ArdevConfigure/main.js
+++ ArdevConfigure/main.js
@@ -1,42 +1,43 @@
-const { app, BrowserWindow } = require("electron");
-const path = require("path");
-const ipc = require("electron").ipcMain;
-var mainWindow;
-var userchoice;
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const ipc = require('electron').ipcMain
+var mainWindow
+var userchoice
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-      enableRemoteModule: true,
-    },
-    minHeight : 800,
-    minWidth : 800,
-  });
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+        },
+        minHeight: 800,
+        minWidth: 800,
+    })
 
-  mainWindow.loadFile("static/index.html");
-  // mainWindow.webContents.openDevTools();
+    mainWindow.loadFile('static/index.html')
+    // mainWindow.webContents.openDevTools();
 }
 
 app.whenReady().then(() => {
-  createWindow();
+    createWindow()
 
-  app.on("activate", function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-});
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
 
-app.on("window-all-closed", function () {
-  if (process.platform !== "darwin") app.quit();
-});
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
 
-ipc.on("go-terminal", (e) => {
-  userchoice = {};
-  mainWindow.loadFile("static/terminal.html");
-});
-ipc.on("save-terminal", (e, arg) => {
-  userchoice.terminal = arg;
-  console.log(userchoice);
-});
+ipc.on('go-terminal', (e) => {
+    userchoice = {}
+    mainWindow.loadFile('static/defaults.html')
+})
+
+ipc.on('save-terminal', (e, arg) => {
+    userchoice.terminal = arg
+    console.log(userchoice)
+})
--- ArdevConfigure/static/src/index.js
+++ ArdevConfigure/static/src/index.js
@@ -1,21 +1,7 @@
-var ipcRenderer = require("electron").ipcRenderer;
-const nextButton = document.getElementById("nextbutton");
-var done = false;
+var ipcRenderer = require('electron').ipcRenderer
+const nextWindow = document.getElementById('nextbutton')
 
-nextButton.addEventListener("click", () => {
-  // Save the info here
-  console.log("clicked");
-  ipcRenderer.send("go-terminal", "terminal");
-});
-var has = false;
-console.log("hello")
-var blinkBanner = document.getElementById("logo_blink");
-console.log(blinkBanner.style.opacity);
-setInterval(()=>{
-  if(blinkBanner.style.opacity == 1){
-    blinkBanner.style.opacity = 0;
-  }
-  else{
-    blinkBanner.style.opacity = 1;
-  }
-}, 400)
+nextWindow.addEventListener('click', () => {
+    console.log('clicked')
+    ipcRenderer.send('go-terminal', 'terminal')
+})
