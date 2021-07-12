d87c2cc6a75c7107c8a5088bf285e97676ba7acc
Satyam Singh <stymsingh@outlook.com>
2021-07-12 16:24:40 +0530

    change tab to 2 spaces instead of 4

--- ArdevConfigure/static/src/index.js
+++ ArdevConfigure/static/src/index.js
@@ -2,6 +2,6 @@ var ipcRenderer = require('electron').ipcRenderer
 const nextWindow = document.getElementById('nextbutton')
 
 nextWindow.addEventListener('click', () => {
-    console.log('clicked')
-    ipcRenderer.send('go-terminal', 'terminal')
+  console.log('clicked')
+  ipcRenderer.send('go-terminal', 'terminal')
 })
